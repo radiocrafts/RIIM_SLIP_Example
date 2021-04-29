44aff511a466f76686a6ed235e1c2f24ad8746b5
FRE <f.eskelund@radiocrafts.com>
2021-04-29 11:00:52 +0100

    First working example

---
+++ Doc/source/walkthrough.rst
@@ -0,0 +1,18 @@
+Walkthrough / Quickstart
+========================
+
+To get the example up and running without any modifications, you can follow this walkthorugh
+
+#. Set up the Linux Computer as described in the vendors documentation (for instance : `<https://www.raspberrypi.org/>`_
+#. Execute the **./Tools/Setup.sh** script
+   - This installs all required dependencies
+#. Run the **./Tools/Start.sh** script
+#. When the script ends, an URL address is printed. Go to this address using a browser.
+#. You are up and running. Now you can observe the data, make modifications and play around.
+
+
+Modifications and customization
+-------------------------------
+
+See the **./Tools/Setup.sh** script for details. It is well documented and should give you a good explanation on what is going on
+
---
+++ Tools/Start.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "Starting example"
+echo "Do you want to upload the platforms to the boards [y/n]?"
+echo "This is only neccesary if not already uploaded."
+read UploadPlatforms
+echo "Do you want to upload ICI applications to the boards [y/n]?"
+echo "This is only neccesary if not already uploaded."
+read UploadICI
+
+echo "What is the serial port of the Border Router? (E.g. /dev/ttyUSB0 - See documentation - troubleshooting for help)"
+read BRPort
+
+if [ $UploadICI == "y" ] || [ $UploadPlatforms == "y" ]
+then
+    if [ $UploadPlatforms == "y" ]
+    then
+    python3 ./Tools/rc1882_bootloader_utility/rc188x_bootloader_utility.py load-image -t 100 -p $BRPort -f Platforms/BR_Platform.bin
+    fi
+
+    if [ $UploadICI == "y" ]
+    then
+    python3 ./Tools/rc1882_bootloader_utility/rc188x_bootloader_utility.py load-image -t 100 -p $BRPort -f Output/BR.bin
+    fi
+
+    echo "What is the port of the Mesh Router? (E.g. /dev/ttyUSB1 - See documentation - troubleshooting for help)"
+    read MRPort
+    if [ $UploadPlatforms == "y" ]
+    then
+    python3 ./Tools/rc1882_bootloader_utility/rc188x_bootloader_utility.py load-image -t 100 -p $MRPort -f Platforms/MR_Platform.bin
+    fi
+
+    if [ $UploadICI == "y" ]
+    then
+    python3 ./Tools/rc1882_bootloader_utility/rc188x_bootloader_utility.py load-image -t 100 -p $MRPort -f Output/MR.bin
+    fi
+fi
+
+echo "Starting SLIP"
+sudo ./StartSlip.sh
+
+# SLIP takes some time to start
+sleep 5
+echo "Press RESET on boards and press <ENTER>"
+read something
+
+echo "Starting Node Red"
+node-red-pi --max-old-space-size=256
+
+echo " "
+echo "************************************************************"
+echo "You can now access Node Red using your browser."
+echo "Open your browser and go to the address ********"
