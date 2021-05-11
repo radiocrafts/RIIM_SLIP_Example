c5388f0c7f273562beee0603789d8ed3ced8f023
FRE <f.eskelund@radiocrafts.com>
2021-05-11 14:45:42 +0200

    Cleanup documentation and installation

--- README.md
+++ README.md
@@ -2,10 +2,33 @@
 
 # RIIM SLIP example 
 
-## Installation
-Download the source, either by cloning the repository or downloading the zip file provided here:
+## Requirements
+|                   |                   |
+| ----------------- | ----------------- |
+| Operating system  | Raspberry Pi OS: 5.10.17-v7+   |
+| Computer/Board    | Raspberry Pi 3    |
+
+## Downloading
+Download the source, either by cloning the repository like this:
+
+`
+git clone https://github.com/RCfesk/RIIM_Slip.git
+`
+
+
+...or downloading the zip file provided here:
 [https://github.com/RCfesk/RIIM_Slip/blob/master/Packages/RIIM_Slip.zip]
 
-## Documentation
+## Installation
+If you downloaded the zip-file, unzip it first.
+
+Then enter the **riim_slip** folder and run the install script
+
+`
+./install.sh
+`
+
+
+## Full Documentation
 Please see the documentation located here:
-[https://github.com/RCfesk/RIIM_Slip/blob/master/Doc/source/conf.py]
+[https://github.com/RCfesk/RIIM_Slip/blob/master/Doc/build/html/index.html]
--- install.sh
+++ install.sh
@@ -1,23 +1,50 @@
 #!/bin/bash
 
+echo " "
+echo "************************************************************"
+echo "It is recommended that you read the documentation before"
+echo "you continue with the installation. It is located in"
+echo "./Doc/build/html/index.html . Do you want to open it now?"
+echo "Press [y/n] and <ENTER> : "
+read ViewDoc
+
+if [ "$ViewDoc" == "y" ]
+then
+    xdg-open ./Doc/build/html/index.html &
+fi
 # This script assumes you need sudo to execute apt install
 # It does not check for already existing installations
 
+
 echo " "
 echo "************************************************************"
-echo "This script will install all mandatory packages"
-echo "Do you also want to install the recommended optional packages (wireshark, microcom, tcpdump)"
-echo "Press [y/n] and <ENTER> : "
-read InstallOptional
+echo "Installing required packages"
+echo " We use sudo for apt install, so your password may be required"
+echo " during installation"
+echo "Press <ENTER> to continue"
+read
 
 # Mandatory packages
 sudo apt install build-essential
 sudo apt install git
 sudo apt install radvd
-sudo apt install python3 pyhton3-pip
+sudo apt install python3 python3-pip
+sudo apt install xdg-utils
 
 # Python packages
-pip3 install --user pycryptodomex pyserial
+pip3 install --user pycryptodomex pyserial sphinxcontrib-mermaid sphinx-rtd-theme
+
+# Generate documentation
+pushd ./Doc
+make html
+popd
+
+
+echo " "
+echo "************************************************************"
+echo "Do you also want to install the recommended optional packages (wireshark, microcom, tcpdump)"
+echo "Press [y/n] and <ENTER> : "
+read InstallOptional
 
 # Optional packages
 if [ "$InstallOptional" == "y" ]
